7e2485c5e787f62964ce4a8de11b020cb40a1a8a
Kayla E <kengelstad16@gmail.com>
2025-02-08 22:26:59 -0600

    display for Candidate Search page

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface ICandidate {
+    name: string;
+    username: string; // always exists
+    location: string;
+    avatarUrl: string; // always exists
+    email: string;
+    htmlUrl: string; // always exists
+    company: string;
+    bio: string;
+}
